660c757b91aea8e3d218f9521e9f0a0fd749e4e8
Freddy Montes <freddymontes@gmail.com>
2019-05-05 10:33:51 -0600

    Nest stories

--- components/src/blocks/Button/Button.stories.tsx
+++ components/src/blocks/Button/Button.stories.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from './index';
 
-storiesOf('Button Normal', module)
+storiesOf('Button/Regular', module)
     .add('normal', () => <Button>Hello Button</Button>)
     .add('secondary', () => <Button secondary>Hello Button</Button>)
     .add('disabled', () => <Button disabled>Hello Button</Button>)
     .add('naked', () => <Button naked>Hello Button</Button>)
 
-storiesOf('Button Tiny', module)
+storiesOf('Button/Tiny', module)
     .add('normal', () => <Button tiny>Hello Button</Button>)
     .add('secondary', () => <Button tiny secondary>Hello Button</Button>)
     .add('disabled', () => <Button tiny disabled>Hello Button</Button>)
